873c066c56912ffda951ebff5b4dcf5055496da5
AlesandraIsla <alesandramempis@gmail.com>
2024-12-03 22:03:28 -0300

    :recycle: refactor: Inclusão das assertivas no test_CT003

--- tests/test_CT003.py
+++ tests/test_CT003.py
@@ -16,6 +16,14 @@ class Test_CT003:
         library_page.click_add_a_song()
         library_page.search_song()
         library_page.add_song_in_playlist()
+        menu_page.go_to_library_page()
+        library_page.select_playlist_menu()
+        library_page.select_playlist("Musicas")
         library_page.remove_music_song()
         library_page.remove_confirm_music()
-
+        menu_page.go_to_home_page()
+        menu_page.go_to_library_page()
+        library_page.select_playlist_menu()
+        library_page.select_playlist("Musicas")
+        song_msg = library_page.verify_song_in_playlist()
+        assert song_msg == "Nothing saved yet", f"A mensagem '{song_msg}' não foi encontrada na interface."
